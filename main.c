d36820cf88007d0861c2bd239e1e86f2446f8d40
inigojimenez <ijb336@alumnos.unican.es>
2026-03-24 12:26:24 +0100

    ++

--- main.c
+++ main.c
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
 fdgjgajg
 printf("Hola mundo");FDJIFSDJOIsfhvadfbadjbioadbjoadbadoiozigvjobisj
-=======
-
-printf("Hola mundo");FDJIFSDJOIsfhvadfbadjbioadbjoadbadoiozigvjobisj
-vbvbxbgfnxfns
->>>>>>> a5493f550e58261f52965df556fcee4881ee8f95
